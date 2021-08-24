332b39c66d7401bd3dd13495bc6c18b633372f66
听雪 <38068739+fgfxf@users.noreply.github.com>
2021-08-24 14:00:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import hashlib
 #pip3 install PySocks -i https://pypi.tuna.tsinghua.edu.cn/simple
 import socket
 import socks
+import json
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
@@ -43,6 +44,34 @@ def testProxies():
     resp=requests.get(url=url)
     print(resp.text)
     return resp.text
+
+def loginPcProxies():
+    url="http://pycn.yapi.3866866.com/login"
+    headers={
+    'POST':'/login HTTP/1.1',
+    'Host':'pycn.yapi.3866866.com',
+    'Content-Length': '47',
+    'Accept': 'text/html, */*; q=0.01',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.62',
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Origin': 'http://pc.py.cn',
+    'Referer': 'http://pc.py.cn/',
+    'Accept-Encoding': 'gzip, deflate',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'Connection': 'close'
+
+    }
+
+    data={
+    'phone':'15238981687',
+    'password':'fmf123456',
+    'remember':0
+    }
+    resp=requests.post(url=url,headers=headers,data=data)
+
+    token=json.loads(resp.text)
+    token=token.get('ret_data').get("token")
+    return token
 
 def addWhite(IP):
     url="http://pycn.yapi.3866866.com/user/save_white_ip"
@@ -291,8 +320,8 @@ checkIP=testProxies()
 ex=r"\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b"
 result=re.findall(ex,checkIP,re.S)
 print(result[0])
-
-addWhite(result[0])
+token=loginPcProxies()
+addWhite(result[0],token)
 setProxies()
 testProxies()
 getformwx(oldcookie)
